493c081949f6aab02449e5e2dc9dd5e5a328a58b
Muhamad Daffa Azfa Rabbani <109969988+Asoatram@users.noreply.github.com>
2025-12-08 18:24:59 +0700

    feat: changed favicon

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Story } from "@/components/Story";
@@ -9,6 +10,7 @@ import { Footer } from "@/components/Footer";
 const Index = () => {
   return (
     <main className="min-h-screen">
+
       <Header />
       <Hero />
       <Story />
